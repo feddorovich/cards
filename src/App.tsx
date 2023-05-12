dc56185b2f775084213afef51c12db104f1edff6
feddorovich <feddorovich@outlook.com>
2023-05-12 16:48:54 +0300

    start cards project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Link} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Main Page</h1>
+      <Link to={'profile'}>profile &nbsp;</Link>
+      <Link to={'login'}>login &nbsp;</Link>
+      <Link to={'registration'}>registration&nbsp;</Link>
+      <Link to={'recovery-password'}>recovery-password&nbsp;</Link>
+      <Link to={'test'}>test</Link>
     </div>
+
   );
 }
 
