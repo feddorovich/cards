5a212e170bc13a447d108b9295409946e0d39da3
feddorovich <feddorovich@outlook.com>
2023-05-26 20:06:18 +0300

    lesson 1 middle

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,58 @@
-import React from "react"
-import logo from "./logo.svg"
 import { Counter } from "features/counter/Counter"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import "./App.css"
+import { store } from "app/store"
+import { Provider } from "react-redux"
+import { createTheme, ThemeProvider } from "@mui/material"
+import { useAppDispatch, useAppSelector } from "app/hooks"
+import { useEffect } from "react"
+import { appActions } from "features/app/app.slice"
+
+export const Test = () => {
+  const isLoading = useAppSelector((state) => state.app.isLoading)
+  const error = useAppSelector((state) => state.app.error)
+  const dispatch = useAppDispatch()
+
+  function handleErrorButtonClicked() {
+    dispatch(appActions.setError({ error: "new error" }))
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch(appActions.setIsLoading({ isLoading: false }))
+    }, 3000)
+  }, [dispatch])
+
+  if (isLoading) return <div>loading...</div>
+  return (
+    <div>
+      <button onClick={handleErrorButtonClicked}>create error</button>
+      {!!error && <h2>{error}</h2>}
+      <Counter />
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Test />,
+    path: "/",
+  },
+  {
+    element: <div>hello</div>,
+    path: "/hello",
+  },
+])
+
+const theme = createTheme()
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a className="App-link" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
-            React
-          </a>
-          <span>, </span>
-          <a className="App-link" href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
-            Redux
-          </a>
-          <span>, </span>
-          <a className="App-link" href="https://redux-toolkit.js.org/" target="_blank" rel="noopener noreferrer">
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a className="App-link" href="https://react-redux.js.org/" target="_blank" rel="noopener noreferrer">
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Provider>
   )
 }
 
---
+++ src/features/app/app.slice.tsx
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+
+const initialAppState = {
+  error: null as string | null,
+  isLoading: true,
+  isAppInitialized: false,
+}
+
+type InitialAppStateType = typeof initialAppState
+
+const slice = createSlice({
+  name: "app",
+  initialState: initialAppState,
+  reducers: {
+    setIsLoading: (state, action: PayloadAction<{ isLoading: boolean }>) => {
+      state.isLoading = action.payload.isLoading
+    },
+    setError: (state, action: PayloadAction<{ error: string | null }>) => {
+      state.error = action.payload.error
+    },
+    setisAppInitialized: (state, action: PayloadAction<{ isAppInitialized: boolean }>) => {
+      state.isAppInitialized = action.payload.isAppInitialized
+    },
+  },
+})
+
+export const appReducer = slice.reducer
+export const appActions = slice.actions
