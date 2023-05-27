d2ba5e416178bb4bbb791b123ed4ca405a90cc25
feddorovich <feddorovich@outlook.com>
2023-05-27 20:31:16 +0300

    replaced react hook form with formik

--- src/features/auth/Login/Login.tsx
+++ src/features/auth/Login/Login.tsx
@@ -1,75 +1,73 @@
-import { useForm, SubmitHandler, Controller } from "react-hook-form"
 import { useAppDispatch } from "app/hooks"
-import { FC, useEffect } from "react"
-import { authThunks } from "features/auth/auth.slice"
-import { ArgLoginType } from "features/auth/auth.api"
+import { FC } from "react"
 import { Checkbox, FormControl, FormControlLabel, FormGroup, Grid, TextField } from "@mui/material"
 import Button from "@mui/material/Button"
 import s from "./Login.module.css"
+import { useFormik } from "formik"
+import { authThunks } from "features/auth/auth.slice"
 
 export const Login: FC = () => {
   const dispatch = useAppDispatch()
 
-  const {
-    getValues,
-    register,
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<ArgLoginType>({
-    defaultValues: {
+  const formik = useFormik({
+    initialValues: {
       email: "",
       password: "",
       rememberMe: false,
     },
+    validate: (values) => {
+      const errors: any = {}
+      if (!values.email) {
+        errors.email = "Required email"
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = "Invalid email address"
+      }
+
+      if (!values.password) {
+        errors.password = "Required pass"
+      } else if (values.password.length < 7) {
+        errors.password = "Must be 7 characters or more"
+      }
+
+      return errors
+    },
+    onSubmit: (values) => {
+      dispatch(authThunks.login(values))
+      formik.resetForm()
+    },
   })
-
-  const onSubmit: SubmitHandler<ArgLoginType> = (data) => {
-    dispatch(authThunks.login(data))
-    console.log(data)
-  }
-
-  // console.log(register())
 
   return (
     <div>
-      {/*<form onSubmit={handleSubmit(onSubmit)}>*/}
-      {/*  <TextField {...register("email")} />*/}
-      {/*  <input defaultValue="fedsygreen@mail.ru" {...register("email")} />*/}
-      {/*  <input defaultValue="12345678" {...register("password", { required: true })} />*/}
-      {/*  {errors.password && <span>This field is required</span>}*/}
-      {/*  <input type="checkbox" {...register("rememberMe")} />*/}
-      {/*  <input type="submit" />*/}
-      {/*</form>*/}
-
       <Grid container justifyContent={"center"}>
         <Grid item justifyContent={"center"}>
           <div className={s.header}>Sign in</div>
-          <form onSubmit={handleSubmit(onSubmit)}>
+          <form onSubmit={formik.handleSubmit}>
             <FormControl>
               <FormGroup>
-                <TextField label="Email" variant="standard" margin="normal" {...register("email")} />
+                <TextField
+                  variant="standard"
+                  label="Email"
+                  margin="normal"
+                  error={!!formik.touched.email && !!formik.errors.email}
+                  helperText={formik.errors.email}
+                  {...formik.getFieldProps("email")}
+                />
                 <TextField
                   type="password"
-                  label="Password"
                   variant="standard"
+                  label="Password"
                   margin="normal"
                   autoComplete={"on"}
-                  {...register("password")}
+                  error={!!formik.touched.password && !!formik.errors.password}
+                  helperText={formik.errors.password}
+                  {...formik.getFieldProps("password")}
                 />
                 <FormControlLabel
-                  label="Remember me"
-                  control={
-                    <Controller
-                      name="rememberMe"
-                      control={control}
-                      render={({ field: { onChange, value } }) => (
-                        <Checkbox checked={value} onChange={(e) => onChange(e.target.checked)} />
-                      )}
-                    />
-                  }
+                  label={"Remember me"}
+                  control={<Checkbox checked={formik.values.rememberMe} {...formik.getFieldProps("rememberMe")} />}
                 />
-                <Button type={"submit"} variant={"contained"} color={"primary"} sx={{ borderRadius: 6 }}>
+                <Button type={"submit"} variant={"contained"} color={"primary"}>
                   Login
                 </Button>
               </FormGroup>
