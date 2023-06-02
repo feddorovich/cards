e4f01be1d304dabf73679d711e8e6cbd26d2e3bf
feddorovich <feddorovich@outlook.com>
2023-06-02 16:11:36 +0300

    add table with data

--- src/features/packs/Packs/Packs.tsx
+++ src/features/packs/Packs/Packs.tsx
@@ -1,11 +1,11 @@
-import React, { FC, useEffect } from "react"
+import React, { FC, useEffect, useState } from "react"
 import { useAppDispatch, useAppSelector } from "common/hooks"
 import { packsThunks } from "features/packs/packs.slice"
-import SortedTable from "features/packs/Packs/Table"
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel } from "@mui/material"
 
 export const Packs: FC = () => {
   const dispatch = useAppDispatch()
-  const cardPacks = useAppSelector((state) => state.packs.cardPacks)
+  const cardPacks = useAppSelector((state) => state.packs.cardPacks.cardPacks)
   const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn)
   // console.log(cardPacks)
 
@@ -15,10 +15,59 @@ export const Packs: FC = () => {
     }
   }, [isLoggedIn])
 
+  const [orderDirection, setOrderDirection] = useState<"asc" | "desc">("asc")
+
+  const handleSortRequest = (): void => {
+    setOrderDirection(orderDirection === "asc" ? "desc" : "asc")
+  }
+
   return (
     <div>
       <div>Packs</div>
-      <SortedTable />
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center" onClick={handleSortRequest}>
+                <TableSortLabel active={true} direction={orderDirection}>
+                  Name
+                </TableSortLabel>
+              </TableCell>
+              <TableCell align="center" onClick={handleSortRequest}>
+                <TableSortLabel active={true} direction={orderDirection}>
+                  Cards
+                </TableSortLabel>
+              </TableCell>
+
+              <TableCell align="center" onClick={handleSortRequest}>
+                <TableSortLabel active={true} direction={orderDirection}>
+                  Last Updated
+                </TableSortLabel>
+              </TableCell>
+              <TableCell align="center" onClick={handleSortRequest}>
+                <TableSortLabel active={true} direction={orderDirection}>
+                  Created by
+                </TableSortLabel>
+              </TableCell>
+              <TableCell align="center">Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {cardPacks &&
+              cardPacks.map((row) => (
+                <TableRow key={row._id}>
+                  <TableCell component="th" scope="row" align="center">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="center">{row.cardsCount}</TableCell>
+                  <TableCell align="center">{row.updated}</TableCell>
+                  <TableCell align="center">{row.created}</TableCell>
+                  <TableCell align="center">Actions</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   )
 }
