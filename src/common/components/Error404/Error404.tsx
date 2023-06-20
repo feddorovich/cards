65965d1b26b96da370febdc1ed9013f2ed064862
feddorovich <feddorovich@outlook.com>
2023-06-20 11:59:14 +0300

    add 404 page navigate

--- src/common/components/Error404/Error404.tsx
+++ src/common/components/Error404/Error404.tsx
@@ -3,14 +3,19 @@ import s from "./Error404.module.css"
 import error404 from "./404.svg"
 import Typography from "@mui/material/Typography"
 import Button from "@mui/material/Button"
+import { useNavigate } from "react-router-dom"
 
 const Error404 = () => {
+  const navigate = useNavigate()
+
   return (
     <div>
       <div className={s.wrapper}>
         <img src={error404} alt={"404"} className={s.error404} />
         <Typography>Page not found</Typography>
-        <Button variant={"outlined"}>Back to home page</Button>
+        <Button variant={"outlined"} onClick={() => navigate("/")}>
+          Back to home page
+        </Button>
       </div>
     </div>
   )
